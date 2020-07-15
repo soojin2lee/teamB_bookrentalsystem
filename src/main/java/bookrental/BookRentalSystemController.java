c5c3648e723c85561991a4f4e2478981325140c1
roy-junny <65518925+roy-junny@users.noreply.github.com>
2020-07-15 11:10:23 +0900

    Add files via upload

--- src/main/java/bookrental/BookRentalSystemController.java
+++ src/main/java/bookrental/BookRentalSystemController.java
@@ -9,7 +9,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@RestController("/rentalapi")
  public class BookRentalSystemController {
 
  @Autowired
